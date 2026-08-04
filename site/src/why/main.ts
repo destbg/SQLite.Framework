a21cc0a011b298e4cff4551f68620878fb38c9e8
Nikolay <35627380+destbg@users.noreply.github.com>
2026-08-04 19:31:58 +0300

    site fixes

--- site/src/why/main.ts
+++
@@ -1,9 +0,0 @@
-import "../shared/tokens.css";
-import "../shared/chrome.css";
-import "../shared/view-transitions.css";
-import "./why.css";
-import { bindThemeToggles } from "../shared/theme";
-import { initReveal } from "../shared/reveal";
-
-bindThemeToggles(".theme-toggle");
-initReveal();
